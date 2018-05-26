17dce38b2d735a3c9a71b8601fdf9bb341c42f49
TK-R <subtkr@gmail.com>
2018-05-26 17:48:32 +0900

    rustソースファイルのビルドをrustcからcargoに変更

--- workspace/rust-sample/src/lib.rs
+++ workspace/rust-sample/src/lib.rs
@@ -1,5 +1,4 @@
 #![feature(lang_items, asm, core_intrinsics)]
-#![crate_type = "staticlib"]
 #![no_std]
 
 #[no_mangle]
@@ -7,16 +6,7 @@ pub extern "C" fn twice(val: u32) -> u32 {
 	let u = val + 2;
 	u
 }
-/*
-#[no_mangle]
-pub extern "C" fn _sbrk() {}
-#[no_mangle]
-pub extern "C" fn _exit() {}
-#[no_mangle]
-pub extern "C" fn _kill() {}
-#[no_mangle]
-pub extern "C" fn _getpid() {}
-*/
+
 #[lang = "eh_personality"]
 extern "C" fn eh_personality() {}
 
---
+++ workspace/rust-sample/target/armv5te-unknown-linux-gnueabi/debug/librust_src.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/TK-R/Documents/ev3rt/hrp2/sdk/workspace/rust-sample/target/armv5te-unknown-linux-gnueabi/debug/librust_src.a: /mnt/c/Users/TK-R/Documents/ev3rt/hrp2/sdk/workspace/rust-sample/src/lib.rs
