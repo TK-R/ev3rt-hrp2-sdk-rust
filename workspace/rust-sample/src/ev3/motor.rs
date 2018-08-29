8ea2bb0acb068c4e3ca0ab9cb1a7d20ca8ac92d1
Ryosuke Koichi <subtkr@gmail.com>
2018-08-29 19:02:14 +0900

    関数呼び出しの差異に、変更が不要な場合には借用を使用するように変更

--- workspace/rust-sample/src/ev3/motor.rs
+++ workspace/rust-sample/src/ev3/motor.rs
@@ -27,7 +27,7 @@ pub enum MotorTypeT {
 }
 
 /// モータのポートを型変換する
-fn get_motor_port(motor_port: MotorPortT) -> u8 {
+fn get_motor_port(motor_port: &MotorPortT) -> u8 {
 	match motor_port {
 		MotorPortT::EV3PortA => 0,
 		MotorPortT::EV3PortB => 1,
@@ -38,7 +38,7 @@ fn get_motor_port(motor_port: MotorPortT) -> u8 {
 }
 
 /// モータのタイプを型変換する
-fn get_motor_type(motor_type: MotorTypeT) -> u8 {
+fn get_motor_type(motor_type: &MotorTypeT) -> u8 {
 	match motor_type {
 		MotorTypeT::NoneMotor => 0,
 		MotorTypeT::MediumMotor => 1,
@@ -50,30 +50,30 @@ fn get_motor_type(motor_type: MotorTypeT) -> u8 {
 
 /// モータポートを設定する。
 /// モータポートに接続しているモータのタイプを決定する。既に設定した場合も新しいモータタイプを指定できる。
-pub fn config(motor_port: MotorPortT, motor_type: MotorTypeT) -> i32 {
-	let motor_port = get_motor_port(motor_port);
-	let motor_type = get_motor_type(motor_type);
+pub fn config(motor_port: &MotorPortT, motor_type: &MotorTypeT) -> i32 {
+	let motor_port = get_motor_port(&motor_port);
+	let motor_type = get_motor_type(&motor_type);
 
 	unsafe { ev3_motor_config(motor_port, motor_type) }
 }
 
 /// モータの角位置を取得する
 /// 不正のモータポート番号をしていた場合、常に0を返す
-pub fn get_counts(motor_port: MotorPortT) -> i32 {
-	let motor_port = get_motor_port(motor_port);
+pub fn get_counts(motor_port: &MotorPortT) -> i32 {
+	let motor_port = get_motor_port(&motor_port);
 	unsafe { ev3_motor_get_counts(motor_port) }
 }
 
 /// モータのパワーを取得する
 /// 不正のモータポート番号を指定した場合、常に0を返す
-pub fn get_power(motor_port: MotorPortT) -> i32 {
-	let motor_port = get_motor_port(motor_port);
+pub fn get_power(motor_port: &MotorPortT) -> i32 {
+	let motor_port = get_motor_port(&motor_port);
 	unsafe { ev3_motor_get_power(motor_port) }
 }
 
 /// モータポートのタイプを取得する
-pub fn get_type(motor_port: MotorPortT) -> MotorTypeT {
-	let motor_port = get_motor_port(motor_port);
+pub fn get_type(motor_port: &MotorPortT) -> MotorTypeT {
+	let motor_port = get_motor_port(&motor_port);
 	unsafe {
 		let motor_type = ev3_motor_get_type(motor_port);
 		match motor_type {
@@ -87,33 +87,33 @@ pub fn get_type(motor_port: MotorPortT) -> MotorTypeT {
 
 /// モータの角位置をゼロにリセットする
 /// モータの角位置センサの値を設定するだけ、モータの実際のパワーと位置に影響を与えない
-pub fn reset_counts(motor_port: MotorPortT) -> i32 {
-	let motor_port = get_motor_port(motor_port);
+pub fn reset_counts(motor_port: &MotorPortT) -> i32 {
+	let motor_port = get_motor_port(&motor_port);
 	unsafe { ev3_motor_reset_counts(motor_port) }
 }
 
 /// モータを指定した角度だけ回転させる
-pub fn rotate(motor_port: MotorPortT, degrees: i32, speed_abs: u32, blocking: bool) -> i32 {
-	let motor_port = get_motor_port(motor_port);
+pub fn rotate(motor_port: &MotorPortT, degrees: i32, speed_abs: u32, blocking: bool) -> i32 {
+	let motor_port = get_motor_port(&motor_port);
 	unsafe { ev3_motor_rotate(motor_port, degrees, speed_abs, blocking) }
 }
 
 /// モータのパワーを設定する
-pub fn set_power(motor_port: MotorPortT, power: i32) -> i32 {
-	let motor_port = get_motor_port(motor_port);
+pub fn set_power(motor_port: &MotorPortT, power: i32) -> i32 {
+	let motor_port = get_motor_port(&motor_port);
 	unsafe { ev3_motor_set_power(motor_port, power) }
 }
 
 /// 2つのモータでロボットのステアリング操作を行う
-pub fn steer(left_motor: MotorPortT, right_motor: MotorPortT, power: i32, turn_ratio: i32) -> i32 {
-	let left_motor = get_motor_port(left_motor);
-	let right_motor = get_motor_port(right_motor);
+pub fn steer(left_motor: &MotorPortT, right_motor: &MotorPortT, power: i32, turn_ratio: i32) -> i32 {
+	let left_motor = get_motor_port(&left_motor);
+	let right_motor = get_motor_port(&right_motor);
 	unsafe { ev3_motor_steer(left_motor, right_motor, power, turn_ratio) }
 }
 
 /// モータを停止する
-pub fn stop(motor_port: MotorPortT, motor_brake: bool) -> i32 {
-	let motor_port = get_motor_port(motor_port);
+pub fn stop(motor_port: &MotorPortT, motor_brake: bool) -> i32 {
+	let motor_port = get_motor_port(&motor_port);
 	unsafe { ev3_motor_stop(motor_port, motor_brake) }
 }
 
--- workspace/rust-sample/src/lib.rs
+++ workspace/rust-sample/src/lib.rs
@@ -48,8 +48,8 @@ fn color_sensor_raw_test(color_sensor_port:SensorPort) {
 		sensor::color_sensor_get_rgb_raw(&color_sensor_port, &mut rgb);
 		lcd::clear(LCDColorT::EV3LCDWhite);
 		lcd::draw_value("Red  \0", rgb.red as i32, "\0", 0, 0 );
-		lcd::draw_value("Green\0", rgb.green as i32, "\0", 0, 12 );
-		lcd::draw_value("Blue \0", rgb.blue as i32, "\0", 0, 24 );
+		lcd::draw_value("Green\0", rgb.green as i32, "\0", 0, 15 );
+		lcd::draw_value("Blue \0", rgb.blue as i32, "\0", 0, 30 );
 		
 		
 		ev3::lap_dly_tsk(100);
@@ -65,7 +65,7 @@ fn touch_sensor_test(touch_sensor_port:SensorPort) {
 	loop {
 		let pressed = sensor::touch_sensor_is_pressed(&touch_sensor_port);
 		lcd::clear(LCDColorT::EV3LCDWhite);
-		lcd::draw_value("Touch\0", pressed as i32, "-\0", 0, 0 );
+		lcd::draw_value("Touch\0", pressed as i32, "-\0", 0, 0);
 		ev3::lap_dly_tsk(100);
 	}
 }
@@ -75,14 +75,14 @@ fn touch_sensor_test(touch_sensor_port:SensorPort) {
 fn button_led_test() {
 	loop {
 		ev3::lap_dly_tsk(100);
-		if button::is_pressed(ButtonT::RightButton) {
-			led::set_led_color(led::LEDColorT::LEDRed);
-		} else if button::is_pressed(ButtonT::LeftButton) {
-			led::set_led_color(LEDColorT::LEDGreen);
-		} else if button::is_pressed(button::ButtonT::UpButton) {
-			led::set_led_color(LEDColorT::LEDOrange);
+		if button::is_pressed(&ButtonT::RightButton) {
+			led::set_led_color(&LEDColorT::LEDRed);
+		} else if button::is_pressed(&ButtonT::LeftButton) {
+			led::set_led_color(&LEDColorT::LEDGreen);
+		} else if button::is_pressed(&ButtonT::UpButton) {
+			led::set_led_color(&LEDColorT::LEDOrange);
 		} else {
-			led::set_led_color(LEDColorT::LEFOff);
+			led::set_led_color(&LEDColorT::LEFOff);
 		}
 	}
 }
@@ -104,23 +104,23 @@ fn battery_test() {
 /// 本体ボタンの押下状態とモータの動作をテストする
 #[allow(dead_code)]
 fn button_motor_test() {
-	motor::config(MotorPortT::EV3PortA, MotorTypeT::LargeMotor);
-	motor::config(MotorPortT::EV3PortB, MotorTypeT::MediumMotor);
+	motor::config(&MotorPortT::EV3PortA, &MotorTypeT::LargeMotor);
+	motor::config(&MotorPortT::EV3PortB, &MotorTypeT::MediumMotor);
 
 	loop {
 		ev3::lap_dly_tsk(100);
 		
-		if button::is_pressed(ButtonT::RightButton) {
-			motor::set_power(MotorPortT::EV3PortA, 50);
-		} else if button::is_pressed(ButtonT::LeftButton) {
-			motor::set_power(MotorPortT::EV3PortA, -50);
-		} else if button::is_pressed(button::ButtonT::UpButton) {
-			motor::set_power(MotorPortT::EV3PortB, 50);
-		} else if button::is_pressed(button::ButtonT::DownButton) {
-			motor::set_power(MotorPortT::EV3PortB, -50);
+		if button::is_pressed(&ButtonT::RightButton) {
+			motor::set_power(&MotorPortT::EV3PortA, 50);
+		} else if button::is_pressed(&ButtonT::LeftButton) {
+			motor::set_power(&MotorPortT::EV3PortA, -50);
+		} else if button::is_pressed(&ButtonT::UpButton) {
+			motor::set_power(&MotorPortT::EV3PortB, 50);
+		} else if button::is_pressed(&ButtonT::DownButton) {
+			motor::set_power(&MotorPortT::EV3PortB, -50);
 		} else {
-			motor::stop(MotorPortT::EV3PortA, false);
-			motor::stop(MotorPortT::EV3PortB, false);
+			motor::stop(&MotorPortT::EV3PortA, false);
+			motor::stop(&MotorPortT::EV3PortB, false);
 		}
 	}
 }
